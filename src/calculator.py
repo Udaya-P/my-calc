e30c03b3c65c57557cf2f280c0c6acc4a00d30d3
Udaya-P <pes2ug23am111@pesu.pes.edu>
2025-10-07 14:55:50 +0530

    Changed multiply and division functions with test cases

--- src/calculator.py
+++ src/calculator.py
@@ -34,6 +34,9 @@ def divide(a, b):
     return result
 
 # TODO: Students will add multiply, divide, power, sqrt functions
+def power(a, b):
+    """power of two numbers"""
+    return a ** b
 
 if __name__ == "__main__":
     print("🧮 Calculator Module")
